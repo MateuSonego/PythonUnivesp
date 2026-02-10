403b65821b4ff23b77f6839f502d0feef2998b42
Salleeem <mateusonego@gmail.com>
2026-02-10 09:40:53 -0300

    Start

---
+++ start/AriTypes.py
@@ -0,0 +1,34 @@
+#Operadores em Python
+
+#Soma 
+x = 3 + 3
+print("Resultado da Soma: " ,  x)
+
+#Subtração 
+x = 3 - 2
+print("Resultado da Subtração: " , x) 
+
+#Multiplicação 
+x = 3 * 3
+print("Resultado da Multiplicação: ", x)
+
+#Divisão 
+x = 9 / 2
+print("Resultado da Divisão: ", x) 
+
+#Divisão Apenas Inteiro 
+x = 9 // 2
+print("Resultado da Divisão(Inteiro): ", x) 
+
+#Divisão Apenas Resto (Módulo)
+x = 9 % 2
+print("Resultado da Divisão(Módulo): ", x) 
+
+#Potenciação
+x = 9 ** 2
+print("Resultado da Potenciação: ", x) 
+
+
+
+
+
---
+++ start/HelloWorld.py
@@ -0,0 +1,3 @@
+# Bem mais fácil do que no Java, por motivos bem obvíos.
+
+print("Hello World!")
